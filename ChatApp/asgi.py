74e72121c2a791de5636a6c2ba6c225d5fae2f92
implicitdefcncdragneel <chandranandan.chandrakar@gmail.com>
2021-07-08 03:09:01 +0530

    jwtMiddlewareAsgi

--- ChatApp/asgi.py
+++ ChatApp/asgi.py
@@ -1,16 +1,55 @@
-"""
-ASGI config for ChatApp project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
-"""
-
 import os
+import jwt
+
 
 from django.core.asgi import get_asgi_application
+from django.contrib.auth.models import AnonymousUser
+from django.contrib.auth import get_user_model
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ChatApp.settings')
+from channels.auth import AuthMiddlewareStack
+from channels.db import database_sync_to_async
+from channels.middleware import BaseMiddleware
 
-application = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+
+from ChatApp import settings
+import api.routing
+
+
+@database_sync_to_async
+def get_user(validated_token):
+    try:
+        payload = jwt.decode(validated_token, settings.SECRET_KEY,algorithms="HS256")
+        user = get_user_model()
+        user = user.objects.get(user_id=payload['user_id'])
+        return user
+    except:
+        return AnonymousUser()
+
+
+class JwtAuthMiddleware(BaseMiddleware):
+    def __init__(self, inner):
+        self.inner = inner
+
+    async def __call__(self, scope, receive, send):
+        query = dict((x.split("=") for x in scope["query_string"].decode().split("&")))
+        scope["user"] = await get_user(validated_token=query.get('token'))
+        return await super().__call__(scope, receive, send)
+
+
+def JwtAuthMiddlewareStack(inner):
+    return JwtAuthMiddleware(AuthMiddlewareStack(inner))
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": JwtAuthMiddlewareStack(
+        URLRouter(
+            api.routing.websocket_urlpatterns
+        )
+    ),
+})
+
