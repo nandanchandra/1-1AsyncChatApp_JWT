9173652b100358662163f395338f446cb9e55755
implicitdefcncdragneel <chandranandan.chandrakar@gmail.com>
2021-07-09 22:10:52 +0530

    userModel

---
+++ accounts/models.py
@@ -0,0 +1,25 @@
+
+import uuid
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+class CustomUser(AbstractUser):
+
+    user_id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False)
+
+    name = models.CharField(max_length=50, default='Anonymous')
+
+    email = models.EmailField(max_length=254, unique=True)
+
+    username = None
+
+    USERNAME_FIELD = 'email'
+
+    REQUIRED_FIELDS = []
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
