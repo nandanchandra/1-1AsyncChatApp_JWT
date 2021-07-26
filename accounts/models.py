65a21513f02965b051047a3c321ec2cde0cc51fd
Chandra Nandan <chandranandan.chandrakar@gmail.com>
2021-07-26 22:12:24 +0530

    requirements.txt

--- accounts/models.py
+++ accounts/models.py
@@ -23,3 +23,6 @@ class User(AbstractUser):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user_id,self.email
